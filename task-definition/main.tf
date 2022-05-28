197ae6b48d18235257c53ecbc2d603094ef9f90d
Cloud-42 <hello@cloud42.io>
2022-05-28 19:55:11 +0000

    Add dynamic volumes block

--- task-definition/main.tf
+++ task-definition/main.tf
@@ -13,5 +13,17 @@ resource "aws_ecs_task_definition" "service" {
   task_role_arn      = var.task_role_arn
   execution_role_arn = var.execution_role_arn
 
+  dynamic "volume" {
+    for_each = var.volumes
+    content {
+      docker_volume_configuration                  = volume["docker_volume_configuration"]
+      efs_volume_configuration                     = volume["efs_volume_configuration"]
+      fsx_windows_file_server_volume_configuration = volume["fsx_windows_file_server_volume_configuration"]
+      host_path                                    = volume["host_path"]
+      name                                         = volume["name"]
+    }
+  }
+
+
   tags = var.tags
 }
