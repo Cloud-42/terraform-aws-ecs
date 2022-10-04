f4f939e8c7a49a1ee8cabba7c4c9010c637535f9
Chris Morgan <chris@cloud42.io>
2022-10-04 13:39:44 +0000

    Optional values no longer experimental

--- service/versions.tf
+++ service/versions.tf
@@ -1,7 +1,6 @@
 terraform {
-  required_version = ">= 1.0.7"
+  required_version = ">= 1.3.1"
   required_providers {
-    aws = ">= 4.20.1"
+    aws = ">= 4.33.0"
   }
-  experiments = [module_variable_optional_attrs]
 }
